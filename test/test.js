9414a1a933ed6b8133c01cdef8d041a3b6ea0669
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2018-08-30 22:48:09 -0700

    cleanup project

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
-const path = require('path')
+// this is not real unit testing, but I made some static websites and tested them
+// in google chrome
+//
+// TODO: write real unit tests
+
 
 const server = require('diet')
-const Static = require('.')
+const Static = require('..')
 const compatible = require('diet-connect')
 const logger = require('morgan')
 
@@ -9,9 +13,9 @@ const app = server()
 
 app.listen('http://localhost:7789')
 
-const _static = Static({ path: path.resolve(__dirname, 'test/static') })
+const _static = Static({ path: app.path + '/static' })
 
-const cuteDocsStatic = Static({ path: path.resolve(__dirname, 'test/cutedocs') })
+const cuteDocsStatic = Static({ path: app.path + '/cutedocs' })
 
 const statics = {
 	simple: _static,
